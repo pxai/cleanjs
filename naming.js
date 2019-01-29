8741bc17da1627d11bd6196a9eeb3f7cdc7ad4a7
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-01-29 08:26:30 +0100

    naming of vars

---
+++ naming.js
@@ -0,0 +1,21 @@
+const variables = () => {
+    var foo = 0;
+    let f = 666;
+
+    let d, e, t = 0;
+
+    let cor = true;
+}
+
+const variables = () => {
+    let counter = 0;
+    let id = 666;
+
+    let directory = "root";
+    let error = 404;
+    let index = 0;
+
+    let i = 0;
+
+    let isCorrect = true;
+}
