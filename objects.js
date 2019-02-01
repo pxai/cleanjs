35e561d5cacac32eed9b4f5c52c4b326e7b2deeb
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-02-01 09:10:14 +0100

    naming of objects

---
+++ objects.js
@@ -0,0 +1,17 @@
+const objectPropertiesBad = () => {
+  const customer = {
+    customerName: "John",
+    customerAge: 12,
+    customerAddress: "Evergreen Terrace 12"
+    customDevice: ["iPhone", "iPad"]
+  }
+}
+
+const objectProperties = () => {
+  const customer = {
+    name: "John",
+    age: 12,
+    address: "Evergreen Terrace 12"
+    devices: ["iPhone", "iPad"]
+  }
+}
