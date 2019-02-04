711b45bb9a29f8e38886a84a66f3413288b94938
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-02-04 22:55:20 +0100

    Ok, done

--- scope.js
+++ scope.js
@@ -1,9 +1,24 @@
 const scopeBad = () => {
   const numbers = [3, 4, 1, 5, 0];
   const odds = numbers.map( (number, index) => number % 2 !== 0);
+
+  numbers.map( (n, i) => {
+    console.log(`Changing ${i} with ${n}`);
+    const r = 0;
+    //...
+    //...
+    console.log(`Result for ${i): ${r});
+  });
 };
 
 const scope = () => {
   const numbers = [3, 4, 1, 5, 0];
   const odds = numbers.map( (n, i) => n % 2 !== 0);
+
+  numbers.map( (number, index) => {
+    console.log(`Changing ${index} with ${number}`);
+    const result = 0;
+    //...
+    //...
+    console.log(`Result for ${index): ${result});
 };
