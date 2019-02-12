2c46dc91a1e025018a48a111bafeb74d69bde48c
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-02-12 23:28:24 +0100

    object init

--- objects.js
+++ objects.js
@@ -15,3 +15,42 @@ const objectProperties = () => {
     devices: ["iPhone", "iPad"]
   }
 }
+
+const defaultObjectsBad = () => {
+  let bad = {
+     id: null,
+     name: "",
+     address: "Nowhere",
+     isVip: false
+   };
+
+   function init (bad) {
+     const customer = {};
+     customer.id = bad.id || 1;
+     customer.name = bad.name || "Default';
+     customer.address = bad.address || "Unknown";
+     customer.isVip = bad.isVip || false;
+   }
+
+   init(bad);
+}
+
+const defaultObjects = () => {
+  let bad = {
+     id: null,
+     name: "",
+     address: "Nowhere",
+     isVip: false
+   };
+
+   function init (bad) {
+    let customer = Object.assign({
+        id: 1,
+        name: "Default",
+        address: "Unknown",
+        isVip: false
+      }, bad);
+   }
+
+   init(bad);
+}
